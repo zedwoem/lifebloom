d6abab8fd36159bc252a84f3939a84d34a76b220
liorazedwoem <liorazedwoem@gmail.com>
2026-05-25 20:57:40 +0700

    Fix i18n and GTranslate conflict by disabling next-intl locale detection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,8 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['en', 'id', 'es', 'fr', 'de'],
   defaultLocale: 'en',
-  localePrefix: 'as-needed'
+  localePrefix: 'as-needed',
+  localeDetection: false
 });
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
